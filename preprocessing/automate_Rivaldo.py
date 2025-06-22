7b0cf0ab48ccbc4bc58dad2505735c6f4b75a8c2
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-06-22 12:00:38 +0700

    update working directory

--- preprocessing/automate_Rivaldo.py
+++ preprocessing/automate_Rivaldo.py
@@ -4,7 +4,14 @@ import os
  
 data = pd.read_csv("co2emissions_raw.csv")
 data.drop(["Make", "Model"], axis=1, inplace=True)
-X_train, X_test, y_train, y_test = preprocess_data(data, 'CO2 Emissions(g/km)', 'scaler.joblib', 'co2emissions_preprocessing/columns.csv')
+
+
+print("Current working directory:", os.getcwd())
+print("Files before saving:", os.listdir())
+
+output_dir = os.path.join(os.getcwd(), "co2emissions_preprocessing")
+os.makedirs(output_dir, exist_ok=True)
+X_train, X_test, y_train, y_test = preprocess_data(data, 'CO2 Emissions(g/km)', 'scaler.joblib', 'os.path.join(output_dir, "columns.csv")')
 
 train_processed = pd.DataFrame(X_train)
 train_processed['target'] = y_train
@@ -13,10 +20,6 @@ test_processed = pd.DataFrame(X_test)
 test_processed['target'] = y_test
 
 
-print("Current working directory:", os.getcwd())
-print("Files before saving:", os.listdir())
 
-output_dir = os.path.join(os.getcwd(), "co2emissions_preprocessing")
-os.makedirs(output_dir, exist_ok=True)
 train_processed.to_csv(os.path.join(output_dir, "train_processed.csv"), index=False)
 test_processed.to_csv(os.path.join(output_dir, "test_processed.csv"), index=False)
